f5fb79ad3fa9004d8217017b630efbbb4389fd4f
Nitesh Singh <niteshsingh.se@gmail.com>
2024-08-24 10:17:34 +0530

    const_var_let

---
+++ basics/const_let_var.js
@@ -0,0 +1,9 @@
+const accountId = 114458
+let accountEmail = "hitshs@hg.com"
+var accountPassword = 322
+accountCity = "Pune"
+let newVar; // Undefined
+
+// accountId = 9
+// console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity, newVar])
